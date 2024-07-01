c72ef02ab074e199db468d5702477f087f9ce106
Kate <skay.katya@gmail.com>
2024-07-01 19:10:13 +0300

    Cypress start

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 20000,
+  watchForFileChanges: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  numTestsKeptInMemory: 1,
+  chromeWebSecurity: false,
+  screenshotsFolder: 'screenshots',
+  videosFolder: 'video',
+  video: false,
+  responseTimeout: 30000,
+
+  e2e: {
+    retries: {
+      runMode: 1,
+      openMode: 1,
+    },
+    setupNodeEvents(on, config) {
+      //on('test:after:run')
+    },
+  },
+});
